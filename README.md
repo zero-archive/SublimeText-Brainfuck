66a481972849c408485564664a3c0c2af624001d
dotzero <mail@dotzero.ru>
2014-05-05 14:57:48 +0400

    Update to SublimeText 3

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 SublimeText-Brainfuck
 =====================
 
-This plugin provides Brainfuck language support for Sublime Text 3
+This is Sublime Text 3 plugin that implement of interpreter for Brainfuck.
+
+## What is Brainfuck?
+
+[Brainfuck](http://en.wikipedia.org/wiki/Brainfuck) is an esoteric programming language noted for its extreme minimalism. The language consists of only eight simple commands and an instruction pointer. It is a Turing tarpit, designed to challenge and amuse programmers, and was not made to be suitable for practical use. It was created in 1993 by Urban Müller.
+
+
+## Usage
+
+To interpret Brainfuck, make selection of Brainfuck code and press keys:
+
+* Linux: ctrl+shift+b
+* Windows: ctrl+shift+b
+* OS X: ctrl+shift+b
+
+
+## Manual installation
+
+Go to the “Packages” directory (Preferences / Browse Packages…). Then clone this repository:
+
+    git clone git://github.com/dotzero/SublimeText-Brainfuck
+
+
+## Examples
+
+    https://github.com/dotzero/SublimeText-Brainfuck/examples/
---
+++ bf.py
@@ -0,0 +1,104 @@
+import sublime
+import sublime_plugin
+import re
+
+VERSION = int(sublime.version())
+
+
+class BrainfuckInterpreterCommand(sublime_plugin.TextCommand):
+    """Implementation of interpreter for Brainfuck"""
+    def run(self, edit):
+        sels = self.view.sel()
+        self.edit = edit
+        self.window = sublime.active_window()
+
+        if sels[0].empty():
+            self.view.run_command('select_all')
+
+        for s in sels:
+            if not s.empty():
+                selected = self.view.substr(s)
+                self.code = re.sub(r'([^><\+\-\.,\[\]])', '', selected)
+
+                if self.code.count('[') != self.code.count(']'):
+                    msg = 'Brainfuck Interpreter: Bad loops count'
+                    sublime.error_message(msg)
+                elif self.code.count(','):
+                    msg = 'Brainfuck Interpreter type value:'
+                    self.window.show_input_panel(msg, '', self.read_input, None, None)
+                else:
+                    self.bf()
+
+    def bf(self):
+        self.cells = {}
+        self.pointer = 0
+        self.buffer = []
+        self.code_pointer = 0
+        self.output = ''
+
+        while self.code_pointer < len(self.code):
+            self.interpret(self.code[self.code_pointer])
+            self.code_pointer += 1
+
+        view = self.window.new_file()
+        view.run_command('brainfuck_result', {'result': self.output})
+
+    def interpret(self, command):
+        if self.pointer not in self.cells:
+            self.cells[self.pointer] = 0
+
+        if command == '>':
+            self.pointer += 1
+
+        elif command == '<':
+            self.pointer -= 1
+
+        elif command == '+':
+            if self.cells[self.pointer] < 255:
+                self.cells[self.pointer] += 1
+
+        elif command == '-':
+            if self.cells[self.pointer] > 0:
+                self.cells[self.pointer] -= 1
+
+        elif command == '.':
+            if self.pointer in self.cells:
+                self.output += chr(self.cells[self.pointer])
+
+        elif command == ',':
+            if self.input != []:
+                self.cells[self.pointer] = self.input.pop(0)
+
+        elif command == '[':
+            if self.cells[self.pointer] == 0:
+                loop = 1
+
+                while (loop > 0) and (self.code_pointer + 1 < len(self.code)):
+                    self.code_pointer += 1
+                    if self.code[self.code_pointer] == '[':
+                        loop += 1
+                    elif self.code[self.code_pointer] == ']':
+                        loop -= 1
+            else:
+                self.buffer.append(self.code_pointer)
+
+        elif command == ']':
+            self.code_pointer = self.buffer.pop() - 1
+
+        return True
+
+    def read_input(self, input):
+        self.input = list(input.encode('ascii', 'ignore'))
+        self.bf()
+
+    def enc(self):
+        if self.view.encoding() == 'Undefined':
+            return self.view.settings().get('default_encoding', 'UTF-8')
+        else:
+            return self.view.encoding()
+
+
+class BrainfuckResultCommand(sublime_plugin.TextCommand):
+    """Print result of Brainfuck interpreter"""
+    def run(self, edit, result):
+        self.view.insert(edit, 0, result)
